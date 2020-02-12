5448f8edeb94a454e86ed76d548f39eba496168f
Kevin Clark <kevin.clark93@gmail.com>
2020-02-12 20:52:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -10,7 +10,7 @@ def state_log(star_date)
 end
 
 def crew
-  ["test","Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
 def greet_crew(crew)
